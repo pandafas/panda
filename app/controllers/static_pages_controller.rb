dae443befb506b9473cf6089aa49b00158895ea1
pandafas <pandafas@gmail.com>
2017-04-15 17:03:07 -0400

    added 'orders' folder to 'views'. added 'index' and 'show' file in the 'orders' folders. created 'landing_page' in 'static_pages'. changed route to go to landing_page instead of index. added 'featured_product' method.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,12 @@
 class StaticPagesController < ApplicationController
   def index
   end
+
+  def landing_page
+	@featured_product = Product.first
+	end
+end
+
+def landing_page
+	@featured_product = Product.first
 end
