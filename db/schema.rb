e697316bfcdbdc83dfbb4f65eba77076959e9554
pandafas <pandafas@gmail.com>
2017-04-25 22:29:50 -0400

    added proper links from 'projects' to 'products', fixed styling within both pages, added 'category' column to 'products' table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420000145) do
+ActiveRecord::Schema.define(version: 20170426013928) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170420000145) do
     t.datetime "updated_at",  null: false
     t.string   "colour"
     t.integer  "price"
+    t.string   "category"
   end
 
 end
