061ba460f6d2ccd9847871bbc744d6795216f8bc
pandafas <pandafas@gmail.com>
2017-05-03 13:50:35 -0400

    added ERB form to contact page. added 'thank you' static page. added POST route to thank you page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/landing_page'
 
+  post 'static_pages/thank_you'
+
   resources :products
   get 'static_pages/projects'
 
@@ -11,7 +13,9 @@ Rails.application.routes.draw do
   get 'static_pages/landing_page'
   root 'static_pages#landing_page'
 
+
   resources :orders, only: [:index, :show, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
