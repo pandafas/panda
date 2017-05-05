eb2a79377e784a66de443fd5b8aa2dda57f0e465
pandafas <pandafas@gmail.com>
2017-05-04 22:31:13 -0400

    finished styling contact form and 'thank_you' page. added 'thank_you' method to controller. added SendGrid plugin.

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,11 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-	:address => "smtp.sendgrind.net",
+	:address => "smtp.sendgrid.net",
 	:port => "587"
 	:authentication => :plain,
-	:user_name => ENV["SENDGRIND_USERNAME"],
-	:paswword => ENV["SENDGRIND_PASSWORD"],
+	:user_name => ENV["SENDGRID_USERNAME"],
+	:paswword => ENV["SENDGRID_PASSWORD"],
 	:domain => "heroku.com",
 	:enable_starttls_auto => true
 }
