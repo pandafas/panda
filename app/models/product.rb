788c3ca405ad3964891b0547894e7ceb16c74ed7
pandafas <pandafas@gmail.com>
2017-05-21 22:38:11 -0400

    added will_paginate gem. added validations to the products and comment model. added and styled pagination is product/_comments.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,15 @@ class Product < ApplicationRecord
 	has_many :orders
 	has_many :comments
 
+	validates :name, presence: true
+	validates :description, presence: true
+	validates :image_url, presence: true
+	validates :colour, presence: true
+	validates :price, numericality: { only_integer: true}
+	validates :category, presence: true
+
+
+
 	def self.search(search_term)
 		Product.where("name LIKE ?", "%#{search_term}%")
 	end
@@ -17,7 +26,6 @@ class Product < ApplicationRecord
 	def average_rating
 		comments.average(:rating).to_f
 	end
-
 
 end
 
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
+require 'will_paginate'
+require 'will_paginate/active_record'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
