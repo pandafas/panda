e3755acfef7102d7c076556c77f2223bc35e2d3b
pandafas <pandafas@gmail.com>
2017-06-01 17:23:58 -0400

    trying to figure out how to send automatic email when new user signs up via devise gem.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :orders
+
+	after_create :send_new_user_email
+	def send_new_user_email
+		UserMailer.send_new_user_message(@user).deliver
+	end
+
+  #private
+  	#def welcome_message
+  		#UserMailer.welcome_message(self).deliver
+  	#end
 end
