855fef020771502e54c4caf3bcdda2c510dc84f3
pandafas <pandafas@gmail.com>
2017-06-01 20:51:04 -0400

    added confirmaable to devise to be able to send automatic email upon sign up with a link to confirm account.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  #  :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 	has_many :orders
 
-	after_create :send_new_user_email
-	def send_new_user_email
-		UserMailer.send_new_user_message(@user).deliver
-	end
 
-  #private
-  	#def welcome_message
-  		#UserMailer.welcome_message(self).deliver
-  	#end
 end
