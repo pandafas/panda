eb71c0a98fefc362576d075184d7519fedfe077c
pandafas <pandafas@gmail.com>
2017-06-01 21:11:10 -0400

    fixed error on heroku push.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20170602001511) do
-=======
-ActiveRecord::Schema.define(version: 20170524004924) do
->>>>>>> master
-
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
     t.text     "body"
