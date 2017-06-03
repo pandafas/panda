8902e2099666116f77cbe8875ede99388c6393c5
pandafas <pandafas@gmail.com>
2017-06-03 13:37:21 -0400

    added rspec gem. got rid of 'test' directory and moved 'mailers/previews' to the 'spec' folder.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20170602001511) do
+
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
     t.text     "body"
