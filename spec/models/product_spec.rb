2c650f503de7901cc2f7f1b7dd9f8497a29faea9
pandafas <pandafas@gmail.com>
2017-06-05 00:00:13 -0400

    finished writing unit test for product spec- to test that the average rating is giving the correct result and validating that when creating a new product, all product params must be entered or it is invalid.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,11 +7,19 @@ describe Product do
 		@product.comments.create!(rating: 1, user: @user, body: "Ugly sign")
 		@product.comments.create!(rating: 3, user: @user, body: "OK quality")
 		@product.comments.create!(rating: 5, user: @user, body: "Beautiful product")
+		expect(Product.new(description: "purple wood round")).not_to be_valid
 	end
 	context "when the product has comments" do
 		it "returns the average rating of all comments" do
 			expect(@product.average_rating).to eq 3
 		end
 	end
+	context "creates invalid product"do
+		it "return is not valid" do
+			expect(Product.new(description: "purple wood round")).not_to be_valid
+		end
+	end
+
+	
 end
 
