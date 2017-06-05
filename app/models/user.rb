fb785aa258959a3eb95d4b23932f9d007881422d
pandafas <pandafas@gmail.com>
2017-06-04 23:30:32 -0400

    deleted merge comments from user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 	has_many :orders
-<<<<<<< HEAD
-
-
-=======
 	has_many :comments
->>>>>>> master
+
 end
