866166f70ab251cce52fd5e9bf5f30415fe5f878
pandafas <pandafas@gmail.com>
2017-06-07 00:36:34 -0400

    completed function tests on remaining controller spec files.

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,1 +1,16 @@
 require 'rails_helper'
+
+describe ProductsController, type: :controller do
+
+	before do
+		@product = Product.create!(name: "wood sign", image_url: "woodsign.jpg", description: "A sign made of wood", colour: "brown", price: "25", category: "signs")
+	end
+
+	context 'GET #index' do
+		
+		it 'user clicks on show' do
+			get :show, params: { id: @product.id }
+			expect(response).to be_ok
+		end
+	end
+end
