4c300218989db58d0faadb2f01faa95e723ae475
pandafas <pandafas@gmail.com>
2017-07-14 23:45:59 -0400

    added more tests to spec/controller/products_controller_spec. started adding methods to the 'orders' controller.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,8 @@ class OrdersController < ApplicationController
 	before_filter :authenticate_user!
 
 	def index
+		@orders = Order.all
+
 	end
 
 	def show
@@ -14,10 +16,22 @@ class OrdersController < ApplicationController
 	end
 
 	def new
+		@order = Order.new
 	end
 
-	def create
-	end
+	#def create
+		#@order = Order.new(order_params)
+
+		#respond_to do |format|
+      		#if order.save
+        		#format.html { redirect_to @order, notice: 'Your order was successfully created.' }
+        		#format.json { render :show, status: :created, location: @order }
+      		#else
+        		#format.html { render :new }
+        		#format.json { render json: @order.errors, status: :unprocessable_entity }
+      		#end
+    	#end
+	#end
 
 	def destroy
 	end
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 describe ProductsController, type: :controller do
 
 	before do
-		@product = FactoryGirl.create(:product)
+		@product = FactoryGirl.create(:product, name: "Wood 1")
+		@product = FactoryGirl.create(:product, name: "Wood 2")
+	#	@admin = FactoryGirl.create(:admin)
+
 	end
 
 	context 'GET #index' do
@@ -11,6 +14,48 @@ describe ProductsController, type: :controller do
 		it 'user clicks on show' do
 			get :show, params: { id: @product.id }
 			expect(response).to be_ok
+		end
+	end
+
+	#context "with params[:q]" do
+	#	it "populates an array matching the search param" do
+	#		get :index, params: {q: "Wood"}
+	#		expect(assigns(:products)).to match_array([wood])
+	#	end
+	#end
+
+	context 'GET #new' do
+		it 'user adds a new product' do
+			get :new
+			expect(assigns(:product)).to be_a_new(Product)
+		end
+
+		it 'renders the new product template' do
+			get :new 
+			expect(response).to render_template :new
+		end
+	end
+
+	context 'POST #create' do
+		it 'users creates a new product with all fields and saves it to the database' do
+			expect{ post :create, params: {product: attributes_for(:product)}}
+		end
+
+		it 'redirects the user to the products#show page' do
+			expect{ post :create, params: {product: attributes_for(:product)}}
+		#	expect(response).to redirect_to(show_product_session_path)
+		end
+
+		it 'user creates new product with invalid attributes' do
+			expect{post :create, params: {product: attributes_for(:product, name: nil)}}
+		#	expect(response).to render_template :new
+		end
+	end
+
+	context 'PUT #update' do
+		it 'users updates a products attributes' do
+			expect(flash[:notice]
+	#		expect alert 'Product was successfully updated'
 		end
 	end
 end
