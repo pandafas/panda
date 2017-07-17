3a1d5568da8c423d93f6fe5122be520383f5ab52
pandafas <pandafas@gmail.com>
2017-07-16 23:02:38 -0400

    finished writing rspec tests for the products controller. fixed minor issues in the navigation of the projects linking to the appropriates product category.

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -5,24 +5,23 @@ describe ProductsController, type: :controller do
 	before do
 		@product = FactoryGirl.create(:product, name: "Wood 1")
 		@product = FactoryGirl.create(:product, name: "Wood 2")
-	#	@admin = FactoryGirl.create(:admin)
-
 	end
 
+
 	context 'GET #index' do
-		
+		it 'responds successfully with an HTTP 200 status code' do
+      	expect(response).to be_success
+      	expect(response).to have_http_status(200)
+      	end
+    end
+
+	context 'GET #show' do
 		it 'user clicks on show' do
 			get :show, params: { id: @product.id }
 			expect(response).to be_ok
+			expect(response).to render_template('show')
 		end
 	end
-
-	#context "with params[:q]" do
-	#	it "populates an array matching the search param" do
-	#		get :index, params: {q: "Wood"}
-	#		expect(assigns(:products)).to match_array([wood])
-	#	end
-	#end
 
 	context 'GET #new' do
 		it 'user adds a new product' do
@@ -36,26 +35,52 @@ describe ProductsController, type: :controller do
 		end
 	end
 
+
 	context 'POST #create' do
-		it 'users creates a new product with all fields and saves it to the database' do
-			expect{ post :create, params: {product: attributes_for(:product)}}
+		before do
+			@product = FactoryGirl.create(:product)
 		end
 
-		it 'redirects the user to the products#show page' do
+		it 'user creates a new product with all fields and saves it to the database' do
 			expect{ post :create, params: {product: attributes_for(:product)}}
-		#	expect(response).to redirect_to(show_product_session_path)
+			expect(response).to be_ok
+		end
+	end
+
+	context 'POST #create' do
+		before do
+			@product = FactoryGirl.build(:product, name: "")
 		end
 
-		it 'user creates new product with invalid attributes' do
-			expect{post :create, params: {product: attributes_for(:product, name: nil)}}
-		#	expect(response).to render_template :new
+		it 'user creates a new product with invalid or missing attributes' do
+			expect(@product).not_to be_valid
 		end
 	end
 
 	context 'PUT #update' do
-		it 'users updates a products attributes' do
-			expect(flash[:notice]
-	#		expect alert 'Product was successfully updated'
+		before do 
+			@product = FactoryGirl.create(:product)
+			@user = FactoryGirl.build(:admin)
+			sign_in @user
 		end
+
+		it "successfully updates product price" do
+      		@attr = { :name => @product.name, :image_url => @product.image_url, :id => @product.id, :price => "35" }
+      		put :update, params: { :id => @product.id, :product => @attr }
+      		@product.reload
+      		expect(@product.price).to eq 35
+    	end
 	end
+
+	context "DELETE #destroy" do
+		before do
+      		@product = FactoryGirl.create(:product)
+      		@user = FactoryGirl.build(:admin)
+      		sign_in @user
+    	end
+
+    	it "should allow admin to delete product" do
+      		expect(delete :destroy, params: {:id => @product} ).to redirect_to(products_url)
+    	end
+  	end
 end
